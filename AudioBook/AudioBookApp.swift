f718ca46db3f05840ff9041626ae5d0ab2073940
bohdan <bogdan.tkachivskiy@gmail.com>
2024-03-19 07:25:08 +0100

    Initial Commit

---
+++ AudioBook/AudioBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioBookApp.swift
+//  AudioBook
+//
+//  Created by Богдан Ткачивський on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
